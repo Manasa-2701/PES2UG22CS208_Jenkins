f15753f3228be32163640b596bdf7ba884a5c45b
Manasa <144662324+Manasa-2701@users.noreply.github.com>
2025-03-13 16:26:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS208\n";
+   cout << "Manasa\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
